6698678ac3bd2e4887ee33d067f52594e8ff308a
connerdouglass <conner.douglass@gmail.com>
2023-08-09 17:31:25 -0400

    fix: fix error in fallback GetIdleTime func

--- idletime_fallback.go
+++ idletime_fallback.go
@@ -3,7 +3,7 @@
 package idletime
 
 import (
-	"errors"
+	"fmt"
 	"runtime"
 	"time"
 )
@@ -11,5 +11,5 @@ import (
 // GetIdleTime gets the amount of time that the machine has been idle. This is the amount of time since a
 // user input of any kind.
 func GetIdleTime() (time.Duration, error) {
-	return time.Duration(0), errors.New("not implemented for this platform: %s-%s", runtime.GOOS, runtime.GOARCH)
+	return time.Duration(0), fmt.Errorf("not implemented for this platform: %s-%s", runtime.GOOS, runtime.GOARCH)
 }
